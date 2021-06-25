08f885be825345c7909ba255fdc6d1fdb078e8d3
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2021-06-25 15:42:55 -0700

    Implementing Redux

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Title = ({ title }) => {
+  const [counter, setCounter] = useState(0)
   return (
     <div>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}> add one</button>
       <h1>{title}</h1>
     </div>
   )
---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+import posts from '../data/posts'
+
+const postReducer = (state = posts, action) => {
+  switch (action.type) {
+  }
+  return state
+}
+
+export default postReducer
